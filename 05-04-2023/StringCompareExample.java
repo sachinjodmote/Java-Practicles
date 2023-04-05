1f99aede5805c63b331a8e532454f24a2b83d125
Sachin Jodmote <111167433+sachinjodmote@users.noreply.github.com>
2023-04-05 21:12:53 +0530

    Create StringCompareExample.java

---
+++ 05-04-2023/StringCompareExample.java
@@ -0,0 +1,17 @@
+// equals()
+// compareTo()
+// ==
+
+public class StringCompareExample{
+	public static void main(String args[])
+	
+	{String s1="Aman";
+	String s2="Aman";
+	String s3=new String("Aman");
+	String s4="Amit";
+	System.out.println(s1.equalsIgnoreCase(s3));
+		System.out.println(s1.equalsIgnoreCase(s4));
+
+	}
+	
+}
